97138a2de47185153d61cba8a6d7dc7e9e05ca38
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2023-08-12 09:59:38 +0200

    unocss config

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig, presetIcons, presetTypography, presetUno } from "unocss";
 
 export default defineConfig({
+  cli: {
+    entry: {
+      outFile: "static/site.css",
+      patterns: ["templates/**/*.html"],
+    },
+  },
   presets: [presetUno({ dark: "media" }), presetIcons(), presetTypography()],
 });
