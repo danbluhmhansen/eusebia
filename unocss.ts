d2279297c692792c983eeb7950427691835b81b3
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2023-08-14 01:16:51 +0200

    node -> deno

--- unocss.ts
+++ unocss.ts
@@ -1,1 +1,33 @@
+import { createGenerator } from "https://esm.sh/@unocss/core@0.55.0";
+import presetUno from "https://esm.sh/@unocss/preset-uno@0.55.0";
+import presetIcons from "https://esm.sh/@unocss/preset-icons@0.55.0";
+import presetTypography from "https://esm.sh/@unocss/preset-typography@0.55.0";
+import { walk } from "https://deno.land/std@0.198.0/fs/walk.ts";
+import { ensureDir } from "https://deno.land/std@0.198.0/fs/ensure_dir.ts";
+
+const uno = createGenerator({
+  presets: [presetUno({ dark: "media" }), presetIcons(), presetTypography()],
+});
+
+let input = "";
+
+for await (const walkEntry of walk("templates")) {
+  if (walkEntry.isFile) {
+    input += await Deno.readTextFile(walkEntry.path);
+  }
+}
+
+const result = await uno.generate(input);
+
+const data = `/* layer: reset */
 *,::after,::before{box-sizing:border-box;border-width:0;border-style:solid;border-color:var(--un-default-border-color,#e5e7eb)}html{line-height:1.5;-webkit-text-size-adjust:100%;-moz-tab-size:4;tab-size:4;font-family:ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"}body{margin:0;line-height:inherit}hr{height:0;color:inherit;border-top-width:1px}abbr:where([title]){text-decoration:underline dotted}h1,h2,h3,h4,h5,h6{font-size:inherit;font-weight:inherit}a{color:inherit;text-decoration:inherit}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:ui-monospace,SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}table{text-indent:0;border-color:inherit;border-collapse:collapse}button,input,optgroup,select,textarea{font-family:inherit;font-feature-settings:inherit;font-variation-settings:inherit;font-size:100%;font-weight:inherit;line-height:inherit;color:inherit;margin:0;padding:0}button,select{text-transform:none}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button;background-color:transparent;background-image:none}:-moz-focusring{outline:auto}:-moz-ui-invalid{box-shadow:none}progress{vertical-align:baseline}::-webkit-inner-spin-button,::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}blockquote,dd,dl,figure,h1,h2,h3,h4,h5,h6,hr,p,pre{margin:0}fieldset{margin:0;padding:0}legend{padding:0}menu,ol,ul{list-style:none;margin:0;padding:0}textarea{resize:vertical}input::placeholder,textarea::placeholder{opacity:1;color:#9ca3af}[role=button],button{cursor:pointer}:disabled{cursor:default}audio,canvas,embed,iframe,img,object,svg,video{display:block;vertical-align:middle}img,video{max-width:100%;height:auto}[hidden]{display:none}
+${result.css}`;
+
+await ensureDir("static");
+
+Deno.writeTextFile(
+  "static/site.css",
+  Deno.args.some((arg) => arg === "--minify" || arg === "-m")
+    ? data.replace(/[\n\r]/g, "")
+    : data,
+);
